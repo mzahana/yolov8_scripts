ebf6a6ed957fbd954df4407bb7bba8b8b049896d
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2024-02-17 23:55:04 +0300

    Add example in scripts/hyperparameter_tuning.py

--- scripts/hyperparameter_tuning.py
+++ scripts/hyperparameter_tuning.py
@@ -39,3 +39,6 @@ model = YOLO('yolov8n.pt')
 
 # Tune hyperparameters on COCO8 for 30 epochs
 model.tune(data='coco8.yaml', epochs=30, iterations=300, optimizer='AdamW', plots=False, save=False, val=False)
+
+#Example: Training using tuned hyperparameters
+# yolo segment train data=/home/mzahana/datasets/Silki/bundle_detection/Bundle_Detection.v6-allclasses-norotation.yolov8/data.yaml model=/home/mzahana/datasets/Silki/bundle_detection/Bundle_Detection.v6-allclasses-norotation.yolov8/runs/segment/tuned/tune/weights/best.pt  optimizer=AdamW epochs=100 imgsz=640 batch=24 optimizer=AdamW  lr0=0.00249 lrf=0.0098 momentum=0.71166 weight_decay=0.00023 warmup_epochs=3.24729 warmup_momentum=0.44252 box=3.32974 cls=0.5337 dfl=1.40185 hsv_h=0.01715 hsv_s=0.54261 hsv_v=0.25993 degrees=0.0 translate=0.0636 scale=0.12643 shear=0.0 perspective=0.0 flipud=0.0 fliplr=0.439 mosaic=0.90559 mixup=0.0 copy_paste=0.0
